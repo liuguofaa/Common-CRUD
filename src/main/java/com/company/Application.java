ae48ee4f1396045ebb572ceee589abb6b3bcd151
GuoFa.Liu <1032855008@qq.com>
2018-11-23 12:18:52 +0800

    增加txmanager

--- src/main/java/com/company/Application.java
+++ src/main/java/com/company/Application.java
@@ -3,6 +3,7 @@ package com.company;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author guofa.liu
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.company"})
 @MapperScan("com.company.mapper")
+@EnableTransactionManagement
 public class Application {
 
     public static void main(String[] args) {
