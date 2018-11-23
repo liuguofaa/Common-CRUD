b5322c367924673506925c3f4d81c287db80c42d
GuoFa.Liu <1032855008@qq.com>
2018-11-23 16:16:06 +0800

    修复

--- src/main/java/com/company/cofiguration/ValidatorConfiguration.java
+++ src/main/java/com/company/cofiguration/ValidatorConfiguration.java
@@ -33,7 +33,7 @@ public class ValidatorConfiguration {
     public Validator validator(){
         ValidatorFactory validatorFactory = Validation.byProvider( HibernateValidator.class )
                 .configure()
-                .addProperty( "hibernate.validator.fail_fast", "false" )
+                .addProperty( "hibernate.validator.fail_fast", "true" )
                 .buildValidatorFactory();
         Validator validator = validatorFactory.getValidator();
 
