da3cc7dee525b2f7d9dc7055d08b1be9b768458e
Your Name <you@example.com>
2018-11-23 11:11:56 +0800

    增加校验

--- src/main/java/com/company/model/Users.java
+++ src/main/java/com/company/model/Users.java
@@ -1,8 +1,12 @@
 package com.company.model;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.company.valid.group.ValidGroupAdd;
+import com.company.valid.group.ValidGroupUpdate;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 /**
@@ -14,10 +18,18 @@ import java.util.Date;
 @Data
 public class Users {
 
+    @NotNull(groups = ValidGroupUpdate.class, message = "id不能为空")
     private Long id;
+
+    @NotBlank(groups = {ValidGroupUpdate.class, ValidGroupAdd.class}, message = "用户名不能为空")
     private String name;
+
+    @NotNull(groups = {ValidGroupUpdate.class, ValidGroupAdd.class}, message = "年龄不能为空")
+    @Range(groups = {ValidGroupUpdate.class, ValidGroupAdd.class}, min = 1,max = 100,message = "年龄不在范围内")
     private Integer age;
+
     private String email;
+
     @TableField("create_time")
     private Date createTime;
 }
